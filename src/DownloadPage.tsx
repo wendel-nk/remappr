9e94272c134d93468f8f1e41670dadf2373a5148
wolffyx <wolffdev@gmail.com>
2026-01-30 14:22:38 +0200

    Fix release issue

--- src/DownloadPage.tsx
+++ src/DownloadPage.tsx
@@ -1,119 +1,119 @@
-import { useEffect, useState } from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {
-    faAndroid,
-    faApple,
-    faLinux,
-    faWindows,
-    IconDefinition,
-} from '@fortawesome/free-brands-svg-icons'
-import { DownloadIcon } from 'lucide-react'
-import releaseData from './data/release-data.json'
+// import { useEffect, useState } from 'react'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import {
+//     faAndroid,
+//     faApple,
+//     faLinux,
+//     faWindows,
+//     IconDefinition,
+// } from '@fortawesome/free-brands-svg-icons'
+// import { DownloadIcon } from 'lucide-react'
+// import releaseData from './data/release-data.json'
 
-type Platform = 'windows' | 'mac' | 'linux' | 'ios' | 'android' | 'unknown'
+// type Platform = 'windows' | 'mac' | 'linux' | 'ios' | 'android' | 'unknown'
 
-const PlatformMetadata: Record<
-    Platform,
-    { name: string; icon: IconDefinition }
-> = {
-    windows: {
-        name: 'Windows',
-        icon: faWindows,
-    },
-    mac: {
-        name: 'macOS',
-        icon: faApple,
-    },
-    linux: {
-        name: 'Linux',
-        icon: faLinux,
-    },
-    ios: {
-        name: 'iOS',
-        icon: faApple,
-    },
-    android: {
-        name: 'Android',
-        icon: faAndroid,
-    },
-    unknown: {
-        name: 'Unknown',
-        icon: faAndroid,
-    },
-}
+// const PlatformMetadata: Record<
+//     Platform,
+//     { name: string; icon: IconDefinition }
+// > = {
+//     windows: {
+//         name: 'Windows',
+//         icon: faWindows,
+//     },
+//     mac: {
+//         name: 'macOS',
+//         icon: faApple,
+//     },
+//     linux: {
+//         name: 'Linux',
+//         icon: faLinux,
+//     },
+//     ios: {
+//         name: 'iOS',
+//         icon: faApple,
+//     },
+//     android: {
+//         name: 'Android',
+//         icon: faAndroid,
+//     },
+//     unknown: {
+//         name: 'Unknown',
+//         icon: faAndroid,
+//     },
+// }
 
-type DownloadLink = {
-    name: string
-    urlPattern: RegExp
-}
+// type DownloadLink = {
+//     name: string
+//     urlPattern: RegExp
+// }
 
-const DownloadLinks: Record<string, DownloadLink> = {
-    windows_exe: {
-        name: 'Windows (exe)',
-        urlPattern: /.*\.exe/,
-    },
-    windows_msi: {
-        name: 'Windows (msi)',
-        urlPattern: /.*\.msi/,
-    },
-    macos: {
-        name: 'macOS',
-        urlPattern: /.*\.dmg/,
-    },
-    linux_appimage: {
-        name: 'Linux (AppImage)',
-        urlPattern: /.*\.AppImage/,
-    },
-    linux_deb: {
-        name: 'Linux (deb)',
-        urlPattern: /.*\.deb/,
-    },
-}
+// const DownloadLinks: Record<string, DownloadLink> = {
+//     windows_exe: {
+//         name: 'Windows (exe)',
+//         urlPattern: /.*\.exe/,
+//     },
+//     windows_msi: {
+//         name: 'Windows (msi)',
+//         urlPattern: /.*\.msi/,
+//     },
+//     macos: {
+//         name: 'macOS',
+//         urlPattern: /.*\.dmg/,
+//     },
+//     linux_appimage: {
+//         name: 'Linux (AppImage)',
+//         urlPattern: /.*\.AppImage/,
+//     },
+//     linux_deb: {
+//         name: 'Linux (deb)',
+//         urlPattern: /.*\.deb/,
+//     },
+// }
 
-const PlatformLinks: Record<Platform, DownloadLink[]> = {
-    windows: [DownloadLinks.windows_exe, DownloadLinks.windows_msi],
-    mac: [DownloadLinks.macos],
-    linux: [DownloadLinks.linux_appimage, DownloadLinks.linux_deb],
-    ios: [],
-    android: [],
-    unknown: [],
-}
+// const PlatformLinks: Record<Platform, DownloadLink[]> = {
+//     windows: [DownloadLinks.windows_exe, DownloadLinks.windows_msi],
+//     mac: [DownloadLinks.macos],
+//     linux: [DownloadLinks.linux_appimage, DownloadLinks.linux_deb],
+//     ios: [],
+//     android: [],
+//     unknown: [],
+// }
 
-const ReleaseAssets = releaseData.assets.map(
-    (asset: any) => asset.browser_download_url,
-)
-const ReleaseVersion = releaseData.tag_name
+// const ReleaseAssets = releaseData.assets.map(
+//     (asset: any) => asset.browser_download_url,
+// )
+// const ReleaseVersion = releaseData.tag_name
 
-function detectPlatform(): Platform {
-    if (typeof window === 'undefined') return 'unknown'
+// function detectPlatform(): Platform {
+//     if (typeof window === 'undefined') return 'unknown'
 
-    const userAgent = window.navigator.userAgent.toLowerCase()
+//     const userAgent = window.navigator.userAgent.toLowerCase()
 
-    if (userAgent.includes('win')) return 'windows'
-    if (userAgent.includes('mac')) return 'mac'
-    if (userAgent.includes('linux')) return 'linux'
-    if (/iphone|ipad|ipod/.test(userAgent)) return 'ios'
-    if (userAgent.includes('android')) return 'android'
+//     if (userAgent.includes('win')) return 'windows'
+//     if (userAgent.includes('mac')) return 'mac'
+//     if (userAgent.includes('linux')) return 'linux'
+//     if (/iphone|ipad|ipod/.test(userAgent)) return 'ios'
+//     if (userAgent.includes('android')) return 'android'
 
-    return 'unknown'
-}
+//     return 'unknown'
+// }
 
-function getUrlFromPattern(assets: string[], pattern: RegExp) {
-    const asset = assets.find((asset) => pattern.test(asset))
-    return asset
-}
+// function getUrlFromPattern(assets: string[], pattern: RegExp) {
+//     const asset = assets.find((asset) => pattern.test(asset))
+//     return asset
+// }
 
 export const Download = () => {
-    const [platform, setPlatform] = useState<Platform>('unknown')
-    const [showAll, setShowAll] = useState(false)
+    // const [platform, setPlatform] = useState<Platform>('unknown')
+    // const [showAll, setShowAll] = useState(false)
 
-    useEffect(() => {
-        const platform = detectPlatform()
-        setPlatform(platform)
-        if (PlatformLinks[platform].length === 0) {
-            setShowAll(true)
-        }
-    }, [])
+    // useEffect(() => {
+    //     const platform = detectPlatform()
+    //     setPlatform(platform)
+    //     if (PlatformLinks[platform].length === 0) {
+    //         setShowAll(true)
+    //     }
+    // }, [])
 
     return (
         <div className="bg-base-200 dark:bg-base-300 text-base-content min-h-full w-full flex flex-col justify-center items-center p-10 pb-48">
@@ -123,8 +123,8 @@ export const Download = () => {
                 className="w-64"
             />
             <div className="text-3xl mb-1">Remappr</div>
-            <div className="text-md mb-1 opacity-70">{ReleaseVersion}</div>
-            <div className="bg-base-100 p-8 max-w-md w-full m-2 rounded-lg shadow-lg dark:shadow-xl">
+            {/* <div className="text-md mb-1 opacity-70">{ReleaseVersion}</div> */}
+            {/* <div className="bg-base-100 p-8 max-w-md w-full m-2 rounded-lg shadow-lg dark:shadow-xl">
                 {PlatformLinks[platform].length > 0 && (
                     <>
                         <div className="flex flex-col gap-3 mb-3">
@@ -180,7 +180,7 @@ export const Download = () => {
                         </div>
                     )}
                 </div>
-            </div>
+            </div> */}
             <a
                 className="text-md hover:underline"
                 href="https://github.com/wolffyx/remappr/releases"
