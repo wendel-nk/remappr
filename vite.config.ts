a5de8f05ac25fac93006d9ab91f6e769282da9c0
wolffyx <wolffdev@gmail.com>
2026-04-26 15:12:45 +0300

    Updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -33,8 +33,11 @@ export default defineConfig({
         outDir: path.resolve(__dirname, 'dist'),
         chunkSizeWarningLimit: 1000, // todo remove after refactoring
         // Tauri uses Chromium on Windows and WebKit on macOS and Linux
-        target:
-            process.env.TAURI_PLATFORM == 'windows' ? 'chrome105' : 'safari13',
+        target: process.env.TAURI_PLATFORM
+            ? process.env.TAURI_PLATFORM == 'windows'
+                ? 'chrome105'
+                : 'safari14'
+            : 'esnext',
         // don't minify for debug builds
         minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
         // produce sourcemaps for debug builds
